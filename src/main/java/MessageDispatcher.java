0132aea3c6a3a6841d355bd6cfcd21dfca8a6827
Jack Wherry <jack@snazz.xyz>
2021-03-10 13:07:03 -0600

    Make global affinity configurable

--- src/main/java/MessageDispatcher.java
+++ src/main/java/MessageDispatcher.java
@@ -55,7 +55,7 @@ public class MessageDispatcher {
 class Trigger {
     private String triggerPhrase = "";
 
-    // Percent chance that the triggerPhrase triggers a response
+    // Chance that the triggerPhrase triggers a response as a fraction of the GLOBAL_AFFINITY config var
     private int affinity = 100;
 
     public int getAffinity() {
@@ -102,7 +102,7 @@ class Trigger {
         if (!test(input)) return null;
 
         // Implement the affinity system so that a response is not issued 100% of the time
-        if (this.affinity < DerpBot.rand.nextInt(100)) return null;
+        if (this.affinity < DerpBot.rand.nextInt(DerpBot.getGlobalAffinity())) return null;
 
         // Implement a lottery system
         // Also potentially inefficient like the searching system
